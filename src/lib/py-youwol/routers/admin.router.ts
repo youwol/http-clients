ca2c86dbe72e13390370796d4eed61334cc119b5
greinisch <greinisch@youwol.com>
2022-02-12 12:00:36 +0100

    ✨ youwol.admin => expose system & localCdn router

--- src/lib/py-youwol/routers/admin.router.ts
+++ src/lib/py-youwol/routers/admin.router.ts
@@ -5,16 +5,22 @@ import { EnvironmentRouter } from './environment/environment.router'
 import { CustomCommandsRouter } from './custom-commands/custom-commands.router'
 import { ProjectsRouter } from './projects/projects.router'
 import { ContextMessage$ } from '../../ws-utils'
+import { SystemRouter } from './system/system.router'
+import { LocalCdnRouter } from './local-cdn/local-cdn.router'
 
 export class AdminRouter extends Router {
     public readonly customCommands: CustomCommandsRouter
     public readonly environment: EnvironmentRouter
     public readonly projects: ProjectsRouter
+    public readonly system: SystemRouter
+    public readonly localCdn: LocalCdnRouter
 
     constructor(parent: Router, ws$: () => ContextMessage$<unknown>) {
         super(parent.headers, `${parent.basePath}/admin`)
         this.customCommands = new CustomCommandsRouter(this)
         this.environment = new EnvironmentRouter(this, ws$)
         this.projects = new ProjectsRouter(this, ws$)
+        this.system = new SystemRouter(this)
+        this.localCdn = new LocalCdnRouter(this, ws$)
     }
 }
