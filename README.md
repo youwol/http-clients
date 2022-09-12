d88ff1df6565a36ae4fccf0850e97318fe83a223
youwol-guillaume <greinisch@youwol.com>
2022-09-12 15:09:28 +0200

    :wrench: update config files

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
-# YouWol HTTP-clients
-
-## Abstract
+# @youwol/http-clients
 
 The library exposes some of YouWol backend services.
+
+This library is part of the hybrid cloud/local ecosystem 
+[YouWol](https://platform.youwol.com/applications/@youwol/platform/latest).
+
+## Links
+
+[Online user-guide](https://l.youwol.com/doc/@youwol/http-clients)
+
+[Developers documentation](https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/http-clients)
+
+[Package on npm](https://www.npmjs.com/package/@youwol/http-clients)
+
+[Source on GitHub](https://github.com/youwol/http-clients)
+
+# Installation, Build, Test
+
+To install the required dependencies:
+
+```shell
+yarn
+```
+---
+To build for development:
+
+```shell
+yarn build:dev
+```
+
+To build for production:
+
+```shell
+yarn build:prod
+```
+---
+Tests require [py-youwol](https://l.youwol.com/doc/py-youwol) to run on port 2001 using the configuration defined [here](https://github.com/youwol/integration-tests-conf).
+
+To run tests:
+```shell
+yarn test
+```
+
+Coverage can be evaluated using:
+```shell
+yarn test-coverage
+```
+---
+
+To generate code's documentation:
+
+```shell
+yarn doc
+```
--- template.py
+++ template.py
@@ -1,15 +1,22 @@
+import shutil
 from pathlib import Path
 
 from youwol.pipelines.pipeline_typescript_weback_npm import Template, PackageType, Dependencies, \
     RunTimeDeps, generate_template
+from youwol_utils import parse_json
+
+folder_path = Path(__file__).parent
+
+pkg_json = parse_json(folder_path / 'package.json')
+
 
 template = Template(
-    path=Path(__file__).parent,
+    path=folder_path,
     type=PackageType.Library,
-    name="@youwol/http-clients",
-    version="0.1.13-wip",
-    shortDescription="The library exposes some of YouWol backend services.",
-    author="greinisch@youwol.com",
+    name=pkg_json['name'],
+    version=pkg_json['version'],
+    shortDescription=pkg_json['description'],
+    author=pkg_json['author'],
     dependencies=Dependencies(
         runTime=RunTimeDeps(
             load={
@@ -17,7 +24,7 @@ template = Template(
             },
         ),
         devTime={
-            "@youwol/cdn-client": "^0.1.3",
+            "@youwol/cdn-client": "^1.0.1",
             "isomorphic-fetch": "^3.0.0",
         }),
     testConfig="https://github.com/youwol/integration-tests-conf",
@@ -25,3 +32,13 @@ template = Template(
     )
 
 generate_template(template)
+shutil.copyfile(
+    src=folder_path / '.template' / 'src' / 'auto-generated.ts',
+    dst=folder_path / 'src' / 'auto-generated.ts'
+)
+for file in ['README.md', '.gitignore', '.npmignore', '.prettierignore', 'LICENSE', 'package.json',
+            'tsconfig.json', 'webpack.config.ts']:
+    shutil.copyfile(
+        src=folder_path / '.template' / file,
+        dst=folder_path / file
+    )
