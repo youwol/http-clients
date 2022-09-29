2de73930c5d96700cb4c7268928288b8e1e40312
youwol-guillaume <greinisch@youwol.com>
2022-09-29 17:09:36 +0200

    :white_check_mark: py-youwol.projects.test => simplify implementation of 'pyYouwol.admin.projects.runStep'

--- src/tests/py-youwol/projects.test.ts
+++ src/tests/py-youwol/projects.test.ts
@@ -1,12 +1,13 @@
+/* eslint-disable jest/no-done-callback -- eslint-comment It is required because */
+
 import { btoa } from 'buffer'
 import { combineLatest, Observable, of } from 'rxjs'
-import { expand, map, mapTo, mergeMap, reduce, take, tap } from 'rxjs/operators'
+import { filter, map, mergeMap, reduce, take, tap } from 'rxjs/operators'
 import { raiseHTTPErrors } from '../../lib'
-import { PyYouwolClient } from '../../lib/py-youwol'
-import { PipelineStepStatusResponse } from '../../lib/py-youwol'
+import { PyYouwolClient, PipelineStepStatusResponse } from '../../lib/py-youwol'
 
 import { expectAttributes } from '../common'
-/* eslint-disable jest/no-done-callback -- eslint-comment Find a good way to work with rxjs in jest */
+
 import '../mock-requests'
 import {
     expectArtifacts$,
@@ -17,7 +18,6 @@ import {
     expectProjectsStatus,
     expectProjectStatus,
     expectPublishLocal,
-    expectPublishRemote,
     setup$,
     uniqueProjectName,
 } from './utils'
@@ -53,6 +53,7 @@ beforeAll(async (done) => {
         })
 })
 
+// eslint-disable-next-line jest/expect-expect -- expects are factorized in test_download_asset
 test('pyYouwol.admin.projects.status', (done) => {
     combineLatest([
         pyYouwol.admin.projects.status$().pipe(raiseHTTPErrors()),
@@ -103,17 +104,15 @@ test('pyYouwol.admin.projects.flowStatus', (done) => {
         })
 })
 
-function run$(
-    stepId: string,
-    expectedCount: number,
-): Observable<PipelineStepStatusResponse> {
+function run$(stepId: string): Observable<PipelineStepStatusResponse> {
     return combineLatest([
         pyYouwol.admin.projects
             .runStep$({ projectId: btoa(projectName), flowId: 'prod', stepId })
             .pipe(raiseHTTPErrors()),
-        pyYouwol.admin.projects.webSocket.pipelineStepStatus$().pipe(
+        pyYouwol.admin.projects.webSocket.pipelineStepStatus$({ stepId }).pipe(
             map((d) => d.data),
-            take(expectedCount),
+            filter((message) => message.status == 'OK'),
+            take(1),
             reduce((acc, e) => [...acc, e], []),
         ),
     ]).pipe(map(([_, respWs]) => respWs.find((step) => step.stepId == stepId)))
@@ -121,46 +120,37 @@ function run$(
 
 test('pyYouwol.admin.projects.runStep', (done) => {
     const projectId = btoa(projectName)
-    const data = [
-        { step: 'init', expectTests: expectInitStep, expectedRefreshCount: 4 },
-        {
-            step: 'build',
-            expectTests: expectBuildStep,
-            expectedRefreshCount: 3,
-        },
-        {
-            step: 'publish-local',
-            expectTests: expectPublishLocal,
-            expectedRefreshCount: 2,
-        },
-        {
-            step: 'publish-remote',
-            expectTests: expectPublishRemote,
-            expectedRefreshCount: 1,
-        },
-    ]
+    const steps$ = expectPipelineStepEvents$(pyYouwol)
 
-    expectPipelineStepEvents$(pyYouwol).subscribe(() => {
+    expectArtifacts$(pyYouwol, projectId)
+    const runs$ = of(0).pipe(
+        mergeMap(() => {
+            return run$('init')
+        }),
+        tap((resp) => {
+            expectInitStep(resp)
+        }),
+        mergeMap(() => {
+            return run$('build')
+        }),
+        tap((resp) => {
+            expectBuildStep(resp)
+        }),
+        mergeMap(() => {
+            return run$('cdn-local')
+        }),
+        tap((resp) => {
+            expectPublishLocal(resp)
+        }),
+        mergeMap(() => {
+            return expectArtifacts$(pyYouwol, projectId)
+        }),
+    )
+    combineLatest([runs$, steps$]).subscribe(([artifacts, steps]) => {
+        expect(artifacts).toBeTruthy()
+        expect(steps).toBeTruthy()
         done()
+        /* no op */
     })
-
-    of(0)
-        .pipe(
-            expand((i) => {
-                const { step, expectTests, expectedRefreshCount } = data[i]
-                return run$(step, expectedRefreshCount).pipe(
-                    tap((stepResp) => expectTests(stepResp)),
-                    mapTo(i + 1),
-                )
-            }),
-            take(4),
-            reduce((acc, e) => [acc, e], []),
-            mergeMap(() => {
-                return expectArtifacts$(pyYouwol, projectId)
-            }),
-        )
-        .subscribe(() => {
-            /* no op */
-        })
 })
 /* eslint-enable jest/no-done-callback -- re-enable */
